919484c035ae516428121d880b9cc114ca94a3eb
Amalia Avramov <amalia.avramov@yahoo.com>
2022-05-08 14:41:27 +0300

    Extract user information from token

--- Web/web/src/pages/Login.js
+++ Web/web/src/pages/Login.js
@@ -3,14 +3,19 @@ import { Grid,Paper, TextField, Button, Typography,Link } from '@mui/material'
 import '../styles/Login.css'
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import jwt from 'jwt-decode'
+import Popup from 'reactjs-popup'
 
 function Login(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [users, setUsers] = useState([]);
     const [token,setToken]=useState('')
+    const [buttonSignIn,setButtonSignIn]=useState(false)
 
     const navigate=useNavigate()
+
+    const closeModal = () => setButtonSignIn(false);
 
     function handleLoginButtonClick () {
         if(username==''){
@@ -27,15 +32,19 @@ function Login(){
             }
             axios.post('http://3.66.157.143/api/Auth/login', user)
                 .then(res => {
-                    setToken(res.data)
-                    console.log(res.data)
-
+                    const token = res.data.token;
+                    const decode = jwt(token);
+                    console.log(decode.roles);
+                    localStorage.setItem('token', token);
+                    if(decode.roles==='admin'){
+                        navigate("/dashboard", {state: username})
+                    }
+                    else setButtonSignIn(btn=>!btn)
                 })
                 .catch(err => {
                     console.log(err)
                 })
 
-            navigate("/dashboard", {state: username})
         }
     }
 
@@ -61,7 +70,16 @@ function Login(){
                         <TextField label='Username' placeholder='Enter username' fullWidth required onChange={handleUsernameChange}/>
                         <TextField label='Password' placeholder='Enter password' type='password' fullWidth required onChange={handlePasswordChange}/>
                         <div className="btnstyle">
-                        <Button onClick={handleLoginButtonClick} type="submit" variant="contained" color="primary">Sign in</Button>
+                            <Button onClick={handleLoginButtonClick} type="submit" variant="contained" color="primary">Sign in</Button>
+                            <Popup className="modal" open={buttonSignIn} closeOnDocumentClick onClose={closeModal}>
+                                <div >
+                                    <a className="modal-close" onClick={closeModal}>
+                                        &times;
+                                    </a>
+                                    <div className="modal-content">You are not an admin!</div>
+
+                                </div>
+                            </Popup>
                         </div>
                         <Typography className="linkToRegister"> Don't have an account?
                             <Link href="/register"  >
