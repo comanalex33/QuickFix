a326489739058d8d75cbad8e04c94eeeda085431
Coman Alex <comanalex33@yahoo.com>
2022-05-19 16:03:09 +0300

    Added possibility to change building as user

--- Mobile/app/src/main/java/app/quic/mobile/services/ApiService.kt
+++ Mobile/app/src/main/java/app/quic/mobile/services/ApiService.kt
@@ -1,17 +1,28 @@
 package app.quic.mobile.services
 
+import app.quic.mobile.models.BuildingModel
 import app.quic.mobile.models.LoginModel
 import app.quic.mobile.models.TokenModel
 import app.quic.mobile.models.UserModel
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface ApiService {
+    //Auth
     @POST("api/auth/login")
     fun login(@Body model: LoginModel): Call<TokenModel>
+
+    //User
     @GET("api/users/{username}")
     fun getUserData(@Path("username") username: String): Call<UserModel>
+    @POST("api/users/{username}/buildings/{id}")
+    fun addBuildingToUser(@Header("Authorization") authHeader : String,
+                          @Path("username") username: String,
+                          @Path("id") id: Long): Call<UserModel>
+
+    //Building
+    @GET("/api/buildings")
+    fun getAllBuildings(): Call<List<BuildingModel>>
+    @GET("api/buildings/{id}")
+    fun getBuildingById(@Path("id") id: Long): Call<BuildingModel>
 }
