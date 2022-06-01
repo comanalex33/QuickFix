106a2c41a8eca7265607e5a22b54b70b688115d1
Sabina <sabina.cenda17@gmail.com>
2022-06-01 21:11:31 +0300

    Added accepted requests on home page

--- Mobile/app/src/main/java/app/quic/mobile/models/RequestModel.kt
+++ Mobile/app/src/main/java/app/quic/mobile/models/RequestModel.kt
@@ -1,6 +1,10 @@
 package app.quic.mobile.models
 
+import app.quic.mobile.services.LoggedInUser
 import java.util.*
 
 class RequestModel (var id: Long, var username: String, var description: String, var roomNumber: String, var cause: String, var category: String, var priority: String, var status: String, var dateTime: String) {
+    fun getRequestStatus(): String? {
+        return status
+    }
 }
